a5cb9b3475e66e2b5a71f517b15cefd94b7568db
Michael Passarge <mpassarge@comcast.net>
2019-11-26 20:12:04 -0500

    Initial Commit

---
+++ nhl/src/main/java/com/passargecorp/nhl/NhlApplication.java
@@ -0,0 +1,13 @@
+package com.passargecorp.nhl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlApplication.class, args);
+	}
+
+}
---
+++ nhl/src/test/java/com/passargecorp/nhl/NhlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passargecorp.nhl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
