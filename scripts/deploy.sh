658be2abd6e763d186a1a610ae6561d6589bbbeb
Michael Passarge <mpassarge@comcast.net>
2020-01-22 21:58:27 -0500

    Removed stagest from pipeline, added deploy, added calling of validation script inside deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
 
-version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+set -e
 
+version=$(mvn help:evaluate -f ../pom.xml -Dexpression=project.version -q -DforceStdout)
+
+echo "Trying to deploy version $version"
+
+./version-validation.sh $version
+
+echo "Logging into Docker"
 echo $DOCKER_TOCKEN | docker login --username $DOCKER_USERNAME --password-stdin
+
+echo "Pushing $version"
 docker push mpassarge/nhl-system:$version
+
+echo "Pushing latest"
 docker push mpassarge/nhl-system:latest
 
+echo "Logging out"
 docker logout
--- scripts/version-validation.sh
+++ scripts/version-validation.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-http_code=$(curl --silent -L -o /dev/null -I --write-out %{http_code} https://hub.docker.com/v2/repositories/mpassarge/nhl-system/tags/$version)
+http_code=$(curl --silent -L -o /dev/null -I --write-out %{http_code} https://hub.docker.com/v2/repositories/mpassarge/nhl-system/tags/$1)
 
 if [ $http_code -eq 200 ]; then
-  echo "Version $version already deployed"
+  echo "Version $1 already deployed"
   exit -1
+else
+  echo "Version, $1, hasn't been deployed, continuing with docker deployment"
 fi
